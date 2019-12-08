caf2284a4a1d9bb1e03628a304847ae0784a31d3
Ahmet Turgut <hahahayesss@gmail.com>
2019-12-08 15:39:56 +0300

    08/12/2019 15:30

--- mk1/Train.py
+++ mk1/Train.py
@@ -4,28 +4,21 @@ import random
 import numpy as np
 import soundfile as sf
 import tensorflow as tf
-from keras.layers import Input, Reshape, Conv2D, MaxPool2D, LSTM, Dropout, UpSampling2D, Activation
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 def _create_rnn():
     model = tf.keras.Sequential()
-    model.add(Input(shape=(210, 210,)))
-    model.add(Conv2D(512, 2, activation="relu"))
-    model.add(Conv2D(512, 2, activation="relu"))
-    model.add(MaxPool2D(2))
+    model.add(tf.keras.layers.Input(shape=(210, 210)))
 
-    model.add(LSTM(512, return_sequences=True))
-    model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(0.2))
-
-    model.add(UpSampling2D(2))
-    model.add(Conv2D(256, 2, activation="relu"))
-    model.add(Conv2D(256, 2, activation="relu"))
-    model.add(Conv2D(210 * 210, 2, activation="relu"))
-    model.add(Reshape((210, 210,)))
-    model.add(Activation("softmax"))
+    model.add(tf.keras.layers.Conv1D(512, 2, strides=2, activation="relu"))
+    model.add(tf.keras.layers.Conv1D(512, 2, strides=2, activation="relu"))
+    model.add(tf.keras.layers.Conv1D(256, 2, strides=2, activation="relu"))
+    model.add(tf.keras.layers.Conv1D(256, 2, strides=2, activation="relu"))
+    model.add(tf.keras.layers.Conv1D(210 * 210, 2, activation="relu"))
+    model.add(tf.keras.layers.Reshape((210, 210)))
+    model.add(tf.keras.layers.Activation("softmax"))
 
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     return model
@@ -63,18 +56,16 @@ def start(input_folder):
     file_list = [f for f in os.listdir(input_folder) if f.__contains__(".ogg")]
     random.shuffle(file_list)
 
-    _piece_size = int(len(file_list) / 8)
+    _piece_size = int(len(file_list) / 100)
     _piece_list = []
     for x in range(0, len(file_list), _piece_size):
         _piece_list.append(file_list[x:(x + _piece_size)])
 
+    model = _create_rnn()
     for _piece in _piece_list:
         _original_list, _noised_list = _preprocess(input_folder, _piece)
+        model.fit(_noised_list, _original_list, batch_size=64, epochs=10)
         print(_original_list.shape, _noised_list.shape)
-
-    model = _create_rnn()
-    # model.fit(_original_list, _noised_list,
-    #           batch_size=64, epochs=10)
 
 
 if __name__ == '__main__':
